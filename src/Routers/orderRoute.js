223e1f5ac763e810fd185e4a4a644a0e491c02f2
ankushshrivastava98 <ankush.shrivastava98@gmail.com>
2023-04-02 21:37:09 +0530

    controller for orders

--- src/Routers/orderRoute.js
+++ src/Routers/orderRoute.js
@@ -1,95 +1,14 @@
 const express = require('express');
-const orderModel = require('./../models/Order');
-
 const router = express.Router();
-
-// [POST]  new Order
-router.post('/order', async (req, res) => {
-    try {
-        const data = new orderModel(req.body);
-        const result = data && await data.save();
-        if (result) {
-            res.json({
-                status: 'SUCCESS',
-                message: 'Order placed successfully.',
-                data: result,
-            })
-        } else {
-            res.json({
-                status: 'FAILED',
-                message: 'Unable to place order, please try again.'
-            })
-        }
-
-    } catch (e) {
-        console.log(e)
-    }
-})
-
-// [GET]  all orders
-router.get('/order', async (req, res) => {
-    try {
-        const result = await orderModel.find()
-        if (result) {
-            res.json({
-                status: 'SUCCESS',
-                message: 'All Orders',
-                count: result.length,
-                data: result,
-            })
-        } else {
-            res.json({
-                status: 'FAILED',
-                message: 'Orders not found.'
-            })
-        }
-    } catch (e) {
-        console.log(e);
-    }
-})
+const { addNewOrder, getAllOrders, deleteOrderById, deleteAllOrders } = require('../controller/orderController');
 
 
-// [Delete] delete single Order
-router.delete('/order/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const result = await orderModel.findByIdAndDelete(id);
-        if (result) {
-            res.json({
-                status: 'SUCCESS',
-                message: 'Order cancelled successfully',
-                data: result,
-            })
-        } else {
-            res.json({
-                status: 'FAILED',
-                message: `Order not found with id: ${id},`
-            })
-        }
-    } catch (e) {
-        console.log(e);
-    }
-})
+router.post('/order', addNewOrder)
 
-// [Delete] delete all Product NOTE: only for developers
-router.delete('/order', async (req, res) => {
-    try {
-        const result = await orderModel.deleteMany({});
-        if (result) {
-            res.json({
-                status: 'SUCCESS',
-                message: 'All Order deleted successfully',
-                data: result,
-            })
-        } else {
-            res.json({
-                status: 'FAILED',
-                message: 'Unable to delete all orders'
-            })
-        }
-    } catch (e) {
-        console.log(e);
-    }
-})
+router.get('/order', getAllOrders)
+
+router.delete('/order/:id', deleteOrderById)
+
+// router.delete('/order', deleteAllOrders)
 
 module.exports = router;
---
+++ src/controller/orderController.js
@@ -0,0 +1,87 @@
+const orderModel = require('./../models/Order');
+
+const addNewOrder = async (req, res) => {
+    try {
+        const data = new orderModel(req.body);
+        const result = data && await data.save();
+        if (result) {
+            res.json({
+                status: 'SUCCESS',
+                message: 'Order placed successfully.',
+                data: result,
+            })
+        } else {
+            res.json({
+                status: 'FAILED',
+                message: 'Unable to place order, please try again.'
+            })
+        }
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const getAllOrders = async (req, res) => {
+    try {
+        const result = await orderModel.find()
+        if (result) {
+            res.json({
+                status: 'SUCCESS',
+                message: 'All Orders',
+                count: result.length,
+                data: result,
+            })
+        } else {
+            res.json({
+                status: 'FAILED',
+                message: 'Orders not found.'
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const deleteOrderById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await orderModel.findByIdAndDelete(id);
+        if (result) {
+            res.json({
+                status: 'SUCCESS',
+                message: 'Order cancelled successfully',
+                data: result,
+            })
+        } else {
+            res.json({
+                status: 'FAILED',
+                message: `Order not found with id: ${id},`
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const deleteAllOrders = async (req, res) => {
+    try {
+        const result = await orderModel.deleteMany({});
+        if (result) {
+            res.json({
+                status: 'SUCCESS',
+                message: 'All Order deleted successfully',
+                data: result,
+            })
+        } else {
+            res.json({
+                status: 'FAILED',
+                message: 'Unable to delete all orders'
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+module.exports = {addNewOrder, getAllOrders, deleteOrderById, deleteAllOrders}
