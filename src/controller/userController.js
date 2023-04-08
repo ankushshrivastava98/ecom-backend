6f5fd05d3229be0be12a71b0cb34b0671d7a8978
ankushshrivastava98 <ankush.shrivastava98@gmail.com>
2023-04-08 18:39:27 +0530

    invalid email check

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,6 +2,7 @@ const userModel = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { badRequest400, serverError500, created201, success200 } = require("../helpers/requestBuilder");
+const validator = require('validator')
 
 const SECRET_KEY = 'ha34523jiosf123dhfj';
 const TOKEN_EXPIRE_TIME = "300s";
@@ -10,28 +11,32 @@ const HASH_SALT = 7;
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
   try {
-    const existingUser = await userModel.findOne({ email });
-    if (existingUser) {
-      badRequest400(res, `User already exists with email: ${email}`)
-    } else {
-      const hashedPassword = await bcrypt.hash(password, HASH_SALT);
-      const newUser = new userModel({
-        username: username || 'User',
-        email: email,
-        password: hashedPassword,
-      });
-      const result = await newUser.save();
-      if (result) {
-        const token = jwt.sign({ email: newUser.email, id: newUser.id }, SECRET_KEY, {
-          expiresIn: TOKEN_EXPIRE_TIME
+    if (validator.isEmail(email)) {
+      const existingUser = await userModel.findOne({ email });
+      if (existingUser) {
+        badRequest400(res, `User already exists with email: ${email}`)
+      } else {
+        const hashedPassword = await bcrypt.hash(password, HASH_SALT);
+        const newUser = new userModel({
+          username: username || 'User',
+          email: email,
+          password: hashedPassword,
         });
-        created201(res, "SIGNNED UP SUCCESSFULLY", { token })
+        const result = await newUser.save();
+        if (result) {
+          const token = jwt.sign({ email: newUser.email, id: newUser.id }, SECRET_KEY, {
+            expiresIn: TOKEN_EXPIRE_TIME
+          });
+          created201(res, "SIGNNED UP SUCCESSFULLY", { token })
+        }
+        else {
+          badRequest400(res, 'Something went wrong');
+        }
       }
-      else {
-        badRequest400(res, 'Something went wrong');
-      }
-
+    } else {
+      badRequest400(res, 'Invalid Email');
     }
+
   } catch (error) {
     console.log(error)
     serverError500(req)
@@ -41,17 +46,21 @@ const signup = async (req, res) => {
 const signin = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const existingUser = await userModel.findOne({ email });
-    if (existingUser) {
-      const passwordMatched = await bcrypt.compare(password, existingUser.password);
-      if (passwordMatched) {
-        const token = jwt.sign({ email: existingUser.email, id: existingUser.id }, SECRET_KEY);
-        success200(res, "SIGNNED IN SUCCESSFULLY", { token })
+    if (validator.isEmail(email)) {
+      const existingUser = await userModel.findOne({ email });
+      if (existingUser) {
+        const passwordMatched = await bcrypt.compare(password, existingUser.password);
+        if (passwordMatched) {
+          const token = jwt.sign({ email: existingUser.email, id: existingUser.id }, SECRET_KEY);
+          success200(res, "SIGNNED IN SUCCESSFULLY", { token })
+        } else {
+          badRequest400(res, 'Invalid Credentials')
+        }
       } else {
-        badRequest400(res, 'Invalid Credentials')
+        badRequest400(res, `User not found with email: ${email}`)
       }
     } else {
-      badRequest400(res, `User not found with email: ${email}`)
+      badRequest400(res, 'Invalid Email');
     }
   } catch (error) {
     console.log(error)
